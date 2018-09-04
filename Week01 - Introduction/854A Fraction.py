d062d97fee7a6139d520e3ed5faba4dbfdc2d1d4
noachnt <42833406+noachnt@users.noreply.github.com>
2018-09-04 20:31:30 +0700

    Update 854A Fraction.py

--- Week01 - Introduction/854A Fraction.py
+++ Week01 - Introduction/854A Fraction.py
@@ -2,11 +2,11 @@ import math
 
 n = int(input())
 
-num = n // 2
-den = n - num
+num = n // 2 #numerator
+den = n - num #denominator
 
-while math.gcd(num, den) != 1:
-			num = num - 1
-			den = den + 1
+while math.gcd(num, den) != 1: 
+			num = num - 1 #numerator + 1
+			den = den + 1 #denominator -1
 			
 print(num, den)
